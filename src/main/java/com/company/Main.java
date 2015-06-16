b523665c234664869eacf3eb84db04670c8c608d
amit <amitdp7@gmail.com>
2015-06-16 18:57:39 +0530

    Initial commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+                System.out.println("Hello Amit ");
+                System.out.println("Hello Sumit ");
+                System.out.println("Hello Monu ");
+                System.out.println("Hello Monu ");
+                System.out.println("Hello Monu ");
+        Pintu
+    }
+}
